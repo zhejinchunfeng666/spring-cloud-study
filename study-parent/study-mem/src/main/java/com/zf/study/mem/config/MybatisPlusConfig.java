18bf8416a0578c7ec9d47574f12a0ea22f92ab9a
ZNL <zhoufang2013@163.com>
2020-05-15 10:27:14 +0800

    MybatisPlus 配置

---
+++ study-parent/study-mem/src/main/java/com/zf/study/mem/config/MybatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.zf.study.mem.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Mybatis-plus分页配置
+ */
+@Configuration
+@ConditionalOnClass(value = {PaginationInterceptor.class})
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+}
